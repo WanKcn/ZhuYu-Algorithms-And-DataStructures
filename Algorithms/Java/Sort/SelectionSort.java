723179dbbc169c9edbcefcb9016b968fb7a6cde3
wankcn <wankcn@icloud.com>
2021-10-22 00:13:47 +0800

    选择排序支持泛型

--- Algorithms/Java/Sort/SelectionSort.java
+++ Algorithms/Java/Sort/SelectionSort.java
@@ -1,5 +1,6 @@
 /**
  * 选择排序
+ *
  * @description: 每一次选择还没处理元素里最小的元素
  * @author: WenRuo
  * @date: 2021/10/20 20:01
@@ -7,8 +8,44 @@
 
 public class SelectionSort {
 
-    private SelectionSort(){ }
-    public static void sort(int[] arr){
+    private SelectionSort() {
+    }
 
+    /**
+     * 升序 Ascending order
+     *
+     * @param arr
+     */
+    public static <E extends Comparable<E>> void sortASC(E[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j].compareTo(arr[minIndex]) < 0)
+                    minIndex = j;
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    /**
+     * 降序 Descending order
+     *
+     * @param arr
+     */
+    public static <E extends Comparable<E>> void sortDESC(E[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j].compareTo(arr[minIndex]) > 0)
+                    minIndex = j;
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    private static <E> void swap(E[] arr, int i, int j) {
+        E tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
