220d4d9ecf545c32bd803fdf1206ef951e9a8477
wankcn <wankcn@icloud.com>
2021-10-24 19:17:28 +0800

    算法性能测试

--- Algorithms/Java/Test/SortingHelper.java
+++ Algorithms/Java/Test/SortingHelper.java
@@ -22,4 +22,23 @@ public class SortingHelper {
         return true;
     }
 
+    /**
+     * 算法性能测试
+     * @param sortName
+     * @param arr
+     * @param <E>
+     */
+    public static <E extends Comparable<E>> void sortTest(String sortName, E[] arr) {
+        long startTime = System.nanoTime();
+        if (sortName.equals("SelectionSort"))
+            SelectionSort.sortASC(arr);
+
+        long endTime = System.nanoTime();
+        double time = (endTime - startTime) / 1000000000.0;
+        if (!SortingHelper.isSorted(arr))
+            throw new RuntimeException(sortName + " failed!");
+        System.out.println(String.format("%s, n = %d : %f s", sortName, arr.length, time));
+
+    }
+
 }
