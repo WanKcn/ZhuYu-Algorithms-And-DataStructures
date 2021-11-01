c82fb2847bd571c127333f72ccb75102a0c41896
文若 <wankang@zorronline.com>
2021-11-01 16:09:16 +0800

    C#版本支持泛型的选择和插入排序

---
+++ Algorithms/C#/Sort/InsertionSort.cs
@@ -0,0 +1,69 @@
+﻿//
+// @Description:
+// @Author: WenRuo
+//
+
+
+using System;
+
+namespace Sort
+{
+    public class InsertionSort
+    {
+        private InsertionSort()
+        {
+        }
+
+        /// <summary>
+        /// 升序未优化
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void Sort1<T>(T[] arr) where T : IComparable
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i; j - 1 >= 0 && arr[j].CompareTo((arr[j - 1])) < 0; j--)
+                    Swap(arr, j, j - 1);
+            }
+        }
+
+        /// <summary>
+        /// 降序
+        /// </summary>
+        /// <returns></returns>
+        public static void Sort2<T>(T[] arr) where T : IComparable
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i; j - 1 >= 0 && arr[j].CompareTo(arr[j - 1]) > 0; j--)
+                    Swap(arr, j, j - 1);
+            }
+        }
+
+        /// <summary>
+        /// 升序优化
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void Sort<T>(T[] arr) where T : IComparable
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                T tmp = arr[i];
+                int j;
+                for (j = i; j - 1 >= 0 && tmp.CompareTo(arr[j - 1]) < 0; j--)
+                    arr[j] = arr[j - 1];
+                arr[j] = tmp;
+            }
+        }
+
+
+        private static void Swap<T>(T[] arr, int i, int j)
+        {
+            T tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+    }
+}
---
+++ Algorithms/C#/Sort/Test.cs
@@ -0,0 +1,27 @@
+﻿//
+// @Description:
+// @Author: WenRuo
+//
+
+using System;
+using System.Diagnostics;
+
+namespace Sort
+{
+    public class Test
+    {
+        static void Main(string[] args)
+        {
+            int[] datas = {1000, 10000, 100000};
+            foreach (var n in datas)
+            {
+                Int32[] arr = ArrayGenerator.GenerateRandomArray(n, n);
+                Int32[] arr2 = new int[arr.Length];
+                Array.Copy(arr, arr2, arr.Length);
+                SortingHelper.SortTest("SelectionSort", arr);
+                SortingHelper.SortTest("InsertionSort", arr2);
+                Console.WriteLine("///");
+            }
+        }
+    }
+}
